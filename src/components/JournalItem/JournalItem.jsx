3073371829ad0d2993edf7fd45fd75a3f3e39c11
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-03-06 19:32:37 +0100

    add JournalItem component

---
+++ src/components/JournalItem/JournalItem.jsx
@@ -0,0 +1,15 @@
+import "./JournalItem.css";
+
+export const JournalItem = ({ title, date, text }) => {
+  const formatedDate = new Intl.DateTimeFormat("pl-PL").format(date);
+
+  return (
+    <>
+      <h2 className="journal-item__header">{title}</h2>
+      <h2 className="journal-item__body">
+        <div className="journal-item__date">{formatedDate}</div>
+        <div className="journal-item__text">{text}</div>
+      </h2>
+    </>
+  );
+};
