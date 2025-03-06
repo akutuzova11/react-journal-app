bdbf81861113647a506238cb255cd54ffa1efd16
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-03-06 19:31:38 +0100

    add JournalAdd component

---
+++ src/components/JournalAdd/JournalAdd.jsx
@@ -0,0 +1,10 @@
+import { CardButton } from "../CardButton/CardButton";
+import "./JournalAdd.css";
+
+export const JournalAdd = () => {
+  return (
+    <CardButton className="journal-add">
+    New memory
+</CardButton>
+  )
+};
