efa338e591357427134345cd3c83623111fecf9f
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-03-06 19:31:03 +0100

    format the code

--- src/components/CardButton/CardButton.jsx
+++ src/components/CardButton/CardButton.jsx
@@ -1,8 +1,7 @@
 import "./CardButton.css";
 
 export const CardButton = ({ children, className }) => {
-
-    const cl = "card-button" + (className ? "" + className : "");
+  const cl = "card-button" + (className ? "" + className : "");
 
   return <button className={cl}>{children}</button>;
 };
