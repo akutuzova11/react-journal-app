0c80b629229f51c8774ee03c0c8bea6a37eebc52
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-03-06 19:28:58 +0100

    implement journal entry structure with layout and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { CardButton } from "./components/CardButton/CardButton";
+import { Header } from "./components/Header/Header";
+import { JournalAdd } from "./components/JournalAdd/JournalAdd";
+import { JournalForm } from "./components/JournalForm/JournalForm";
+import { JournalItem } from "./components/JournalItem/JournalItem";
+import { JournalList } from "./components/JournalList/JournalList";
+import { LeftPanel } from "./layout/LeftPanel/LeftPanel";
+import { RightPanel } from "./layout/RightPanel/RightPanel";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const data = [
+    {
+      title: "Get ready for course renewal",
+      date: new Date(),
+      text: "Simon Walker had always loved industrial Berlin with its bad, black beaches.",
+    },
+    {
+      title: "Mountain rail",
+      date: new Date(),
+      text: "They looked at each other with jumpy feelings...",
+    },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <LeftPanel>
+        <Header />
+        <JournalAdd />
+        <JournalList>
+          <CardButton>
+            <JournalItem
+              title={data[0].title}
+              date={data[0].date}
+              text={data[0].text}
+            />
+          </CardButton>
+          <CardButton>
+            <JournalItem
+              title={data[1].title}
+              date={data[1].date}
+              text={data[1].text}
+            />
+          </CardButton>
+        </JournalList>
+      </LeftPanel>
+      <RightPanel>
+        <JournalForm />
+      </RightPanel>
+    </div>
+  );
 }
 
-export default App
+export default App;
