63cb0d5a1fb1c22aeadbca23b8213f4a12cb3b48
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-03-07 14:21:18 +0100

    add sorting function to order entries by date (newest first) and integrate CardButton and JournalItem components for structured display

--- src/components/JournalList/JournalList.jsx
+++ src/components/JournalList/JournalList.jsx
@@ -1,5 +1,29 @@
+import { CardButton } from "../CardButton/CardButton";
+import { JournalItem } from "../JournalItem/JournalItem";
 import "./JournalList.css";
 
-export const JournalList = ({ children }) => {
-  return <div className="journal-list">{children}</div>;
+export const JournalList = ({ items }) => {
+  if (items.length === 0) {
+    return <p>No memories, add the first one</p>;
+  }
+
+  const sortItems = (a, b) => {
+    if (a.date < b.date) {
+      return 1;
+    } else return -1;
+  };
+
+  return (
+    <div className="journal-list">
+      {items.sort(sortItems).map((element) => (
+        <CardButton key={element.id}>
+          <JournalItem
+            title={element.title}
+            date={element.date}
+            text={element.text}
+          />
+        </CardButton>
+      ))}
+    </div>
+  );
 };
