1afa1126a62cca5efd5d23fc99cbeda4374d72bc
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-03-07 21:23:33 +0100

    implement Form Handling for title, date, post and tag: prevent default submission, validate input fields, and call onSubmit only if all fields are valid.

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,6 +1,7 @@
-import "./JournalForm.css";
+import styles from "./JournalForm.module.css";
 import { Button } from "../Button/Button";
 import { useState } from "react";
+import classNames from "classnames";
 
 export const JournalForm = ({ onSubmit }) => {
   const [formValidState, setFormValidState] = useState({
@@ -44,24 +45,46 @@ export const JournalForm = ({ onSubmit }) => {
   };
 
   return (
-    <form className="journal-form" onSubmit={addJournalItem}>
-      <input
-        type="text"
-        name="title"
-        style={{ border: formValidState.title ? "none" : "1px solid red" }}
-      />
-      <input
-        type="date"
-        name="date"
-        style={{ border: formValidState.date ? "none" : "1px solid red" }}
-      />
-      <input type="text" name="tag" />
+    <form className={styles["journal-form"]} onSubmit={addJournalItem}>
+      <div>
+        <input
+          type="text"
+          name="title"
+          className={classNames(styles["input-title"], {
+            [styles["invalid"]]: !formValidState.title,
+          })}
+        />
+      </div>
+
+      <div className={styles["form-row"]}>
+        <label htmlFor="date" className={styles["form-label"]}>
+          <span>Date</span>
+        </label>
+        <input
+          type="date"
+          name="date"
+          id="date"
+          className={classNames(styles["input"], {
+            [styles["invalid"]]: !formValidState.date,
+          })}
+        />
+      </div>
+
+      <div className={styles["form-row"]}>
+        <label htmlFor="tag" className={styles["form-label"]}>
+          <span>Tags</span>
+        </label>
+        <input type="text" name="tag" id="tag" className={styles["input"]} />
+      </div>
+
       <textarea
         name="post"
         id=""
         cols="30"
         rows="10"
-        style={{ border: formValidState.text ? "none" : "1px solid red" }}
+        className={classNames(styles["input"], {
+          [styles["invalid"]]: !formValidState.post,
+        })}
       ></textarea>
       <Button text="Save" />
     </form>
