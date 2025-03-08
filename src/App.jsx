a7f3a35c431da35fcb2c550575e9c8e58822c60f
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-03-08 18:09:52 +0100

    implement`useEffect` to load and persist journal data in `localStorage`

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Header } from "./components/Header/Header";
 import { JournalAdd } from "./components/JournalAdd/JournalAdd";
 import { JournalForm } from "./components/JournalForm/JournalForm";
@@ -7,23 +7,26 @@ import { LeftPanel } from "./layout/LeftPanel/LeftPanel";
 import { RightPanel } from "./layout/RightPanel/RightPanel";
 import styles from "./App.module.css";
 
-const data = [
-  {
-    id: 1,
-    title: "Get ready for course renewal",
-    date: new Date(),
-    text: "Simon Walker had always loved industrial Berlin with its bad, black beaches.",
-  },
-  {
-    id: 2,
-    title: "Mountain rail",
-    date: new Date(),
-    text: "They looked at each other with jumpy feelings...",
-  },
-];
-
 function App() {
-  const [items, setItems] = useState(data);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("data"));
+    if (data) {
+      setItems(
+        data.map((item) => ({
+          ...item,
+          date: new Date(item.date),
+        }))
+      );
+    }
+  }, []);
+
+  useEffect(() => {
+    if (items.length) {
+      localStorage.setItem("data", JSON.stringify(items));
+    }
+  }, [items]);
 
   const addItem = (item) => {
     setItems((prev) => [
@@ -31,7 +34,7 @@ function App() {
       {
         title: item.title,
         date: new Date(item.date),
-        text: item.text,
+        post: item.post,
         id: Math.max(...prev.map((item) => item.id)) + 1,
       },
     ]);
