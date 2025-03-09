0e62b1994443cc16341a2f97a36c32d18965dd12
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-03-09 14:56:47 +0100

    add SelectUser component to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
+import { SelectUser } from "../SelectUser/SelectUser";
 import styles from "./Header.module.css";
 
 export const Header = () => {
+
   return (
-    <div className={styles.logo}>Logo</div>
-  )
+    <>
+      <div className={styles.logo}>Logo</div>
+      <SelectUser />
+    </>
+  );
 };
