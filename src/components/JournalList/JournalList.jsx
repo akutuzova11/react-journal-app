18c8e6768fe9386624ae275bd2d6c164f340df5c
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-03-09 14:59:11 +0100

    implement user filtering in JournalList based on userId from context

--- src/components/JournalList/JournalList.jsx
+++ src/components/JournalList/JournalList.jsx
@@ -1,8 +1,12 @@
+import { useContext } from "react";
 import { CardButton } from "../CardButton/CardButton";
 import { JournalItem } from "../JournalItem/JournalItem";
 import "./JournalList.css";
+import { UserContext } from "../../context/userContext";
 
-export const JournalList = ({ items=[] }) => {
+export const JournalList = ({ items = [] }) => {
+  const { userId } = useContext(UserContext);
+
   if (!items.length) {
     return <p>No memories, add the first one</p>;
   }
@@ -15,15 +19,18 @@ export const JournalList = ({ items=[] }) => {
 
   return (
     <div className="journal-list">
-      {items.sort(sortItems).map((element) => (
-        <CardButton key={element.id}>
-          <JournalItem
-            title={element.title}
-            date={element.date}
-            text={element.text}
-          />
-        </CardButton>
-      ))}
+      {items
+        .filter((element) => element.userId === userId)
+        .sort(sortItems)
+        .map((element) => (
+          <CardButton key={element.id}>
+            <JournalItem
+              title={element.title}
+              date={element.date}
+              post={element.post}
+            />
+          </CardButton>
+        ))}
     </div>
   );
 };
