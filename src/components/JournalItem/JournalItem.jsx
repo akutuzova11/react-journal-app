5d94a548edca07684f134f4f3b3d7c4f58ad37be
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-03-10 20:15:48 +0100

    use css modules for styling

--- src/components/JournalItem/JournalItem.jsx
+++ src/components/JournalItem/JournalItem.jsx
@@ -1,14 +1,14 @@
-import "./JournalItem.css";
+import styles from "./JournalItem.module.css";
 
 export const JournalItem = ({ title, date, post }) => {
   const formatedDate = new Intl.DateTimeFormat("pl-PL").format(date);
 
   return (
     <>
-      <h2 className="journal-item__header">{title}</h2>
-      <h2 className="journal-item__body">
-        <div className="journal-item__date">{formatedDate}</div>
-        <div className="journal-item__post">{post}</div>
+      <h2 className={styles["journal-item__header"]}>{title}</h2>
+      <h2 className={styles["journal-item__body"]}>
+        <div className={styles["journal-item__date"]}>{formatedDate}</div>
+        <div className={styles["journal-item__post"]}>{post}</div>
       </h2>
     </>
   );
