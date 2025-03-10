166fc744870203b0a69fbc14bba6a231a2fc743a
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-03-10 13:21:40 +0100

    replace prop logic with value for Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,9 @@
 import "./Button.css";
 
-export const Button = ({ text, onClick }) => {
-  return <button onClick={onClick} className="button accent">{text}</button>;
+export const Button = ({ onClick }) => {
+  return (
+    <button onClick={onClick} className="button accent">
+      Save
+    </button>
+  );
 };
