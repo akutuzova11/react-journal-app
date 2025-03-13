bb147f267bcaf1ad63409b4c557b96cf59fa6585
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-03-13 10:46:15 +0100

    enhance text and add image when there are no memories

--- src/components/JournalList/JournalList.jsx
+++ src/components/JournalList/JournalList.jsx
@@ -3,12 +3,18 @@ import { CardButton } from "../CardButton/CardButton";
 import { JournalItem } from "../JournalItem/JournalItem";
 import styles from "./JournalList.module.css";
 import { UserContext } from "../../context/userContext";
+import AddNewMemory from "../../assets/AddNewMemories9.png";
 
 export const JournalList = ({ items = [], setItem }) => {
   const { userId } = useContext(UserContext);
 
   if (!items.length) {
-    return <p>No memories, add the first one</p>;
+    return (
+      <div className={styles["journal-memories"]}>
+        <p className={styles["journal-text"]}>Every great story starts with a first line ...</p>
+        <img className={styles["journal-image"]} src={AddNewMemory} alt="add new memory" />
+      </div>
+    );
   }
 
   const sortItems = (a, b) => {
