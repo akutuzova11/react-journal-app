43eed30a9cf09bed4dbd9f798fa07f73c2e8c9d8
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-03-13 13:04:01 +0100

    remove loading from Save button and disabled prop

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,35 +1,16 @@
-import { useState, useCallback } from "react";
 import styles from "./Button.module.css";
-import { LoadingOverlay } from "../LoadingOverlay/LoadingOverlay.jsx"; // Create this component
 
-export const Button = ({ onClick, ...props }) => {
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleClick = useCallback(
-    async (event) => {
-      setIsLoading(true);
-      try {
-        if (onClick) {
-          await onClick(event);
-        }
-        await new Promise((resolve) => setTimeout(resolve, 1800));
-      } finally {
-        setIsLoading(false);
-      }
-    },
-    [onClick]
-  );
+export const Button = ({ onClick, disabled, ...props }) => {
 
   return (
     <>
       <button
-        onClick={handleClick}
+        onClick={onClick}
         className={`${styles.button} ${styles.accent}`}
-        {...props}
+        {...props} disabled={disabled}
       >
         Save
       </button>
-      {isLoading && <LoadingOverlay />}
     </>
   );
 };
