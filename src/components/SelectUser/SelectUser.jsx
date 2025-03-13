977199cc5e3b703ad4ceb095320043644c0bfb12
Angelina Kutuzova <a.kutuzova11@gmail.com>
2025-03-13 12:13:47 +0100

    replace select and option elements with div tags for custom dropdown styling and implement  dropdown closure on clicks outside the component

--- src/components/SelectUser/SelectUser.jsx
+++ src/components/SelectUser/SelectUser.jsx
@@ -1,23 +1,47 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { UserContext } from "../../context/userContext";
 import styles from "./SelectUser.module.css";
 
 export const SelectUser = () => {
   const { userId, setUserId } = useContext(UserContext);
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
 
-  const changeUser = (event) => {
-    setUserId(Number(event.target.value));
+  const toggleDown = () => setIsOpen(!isOpen);
+
+  const changeUser = (id) => {
+    setUserId(id);
+    setIsOpen(false);
   };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  const selectedUser =
+  userId === 1 ? "User Profile: Anna" : "User Profile: Mark";
+
   return (
-    <select
-      className={styles["select"]}
-      name="user"
-      value={userId}
-      id="user"
-      onChange={changeUser}
-    >
-      <option value="1">Anna</option>
-      <option value="2">Mark</option>
-    </select>
+    <div className={styles["custom-dropdown"]} ref={dropdownRef}>
+      <div className={styles["selected"]} onClick={toggleDown}>
+        {selectedUser}
+      </div>
+      {isOpen && (
+        <div className={styles["dropdown-options"]}>
+          <div className={styles["option"]} onClick={() => changeUser(1)}>
+            Anna
+          </div>
+          <div className={styles["option"]} onClick={() => changeUser(2)}>
+            Mark
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
